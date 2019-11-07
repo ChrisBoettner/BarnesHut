eff7423d7ac0bef10e257e89c290995a822682ca
WjCron <christopherboettner@yahoo.de>
2019-11-07 12:51:27 +0100

    Try to implement Foce Calculation
    Doesnt quite work yet. Something is going wrong in the recursive function

--- Force.py
+++ Force.py
@@ -4,4 +4,39 @@ Calculating the Force on a particle
 
 """
 
-import QuadTree
+from math import sqrt
+
+def Force(x, y, Tree, theta):
+    TotFx = 0
+    TotFy = 0
+    
+    
+    return(Fx,Fy)
+    
+    
+    # doesnt yet check if node is the same as the body itself
+def CheckNode(x ,y, node, theta):
+    if not node.children:
+        if not node.points:
+            return
+        else:
+            return(TwoBodyAcc(x, y, node.ComX, node.ComY, node.TotM))
+    else:
+        d = sqrt((x-node.ComX)**2+(y-node.ComY)**2)
+        if node.width/d < theta:
+            return(TwoBodyAcc(x, y, node.ComX, node.ComY, node.TotM))
+        else:
+            CheckNode(x, y, node.children[0], theta)
+            CheckNode(x, y, node.children[1], theta)
+            CheckNode(x, y, node.children[2], theta)
+            CheckNode(x, y, node.children[3], theta)
+
+
+def TwoBodyAcc(x1, y1, x2, y2, m2):
+    """
+    Acceleration of body 1
+    """
+    G = 1
+    Fx = G*m2*1./sqrt((x2-x1)**2+(y2-y1)**2)**3*(x2-x1)
+    Fy = G*m2*1./sqrt((x2-x1)**2+(y2-y1)**2)**3*(y2-y1)
+    return(Fx,Fy)
--- QuadTree.py
+++ QuadTree.py
@@ -2,9 +2,11 @@
 Creating the QuadTree
 
 """
-import numpy as np
+from numpy import amax, amin
 import matplotlib.pyplot as plt
 from matplotlib import patches
+
+import Force
 
 # =============================================================================
 # Classes
@@ -12,9 +14,17 @@ from matplotlib import patches
 
 
 class Body():
-    def __init__(self, x, y):
+    def __init__(self, x, y, vx=None, vy=None, m=None):
         self.x = x
         self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.m  = m
+    
+    def Calc_Acc(self, QTree, theta):
+        Ax, Ay = Force(self.x,self.y, QTree, theta)
+        return(Ax,Ay)
+        
 
 class Node():
     def __init__(self, x0, y0, w, h, points):
@@ -24,28 +34,40 @@ class Node():
         self.height = h
         self.points = points
         self.children = []
+    
+        self.ComX, self.ComY, self.TotM = self.Com()
+        
+    def Com(self):
+        if not self.points:
+            return(None,None,None)
+        else:
+            TotM = sum([self.points[n].m for n in range(len(self.points))])
+            ComX = 1./TotM * sum([self.points[n].m*self.points[n].x for n in range(len(self.points))])
+            ComY = 1./TotM * sum([self.points[n].m*self.points[n].y for n in range(len(self.points))])
+            return(ComX,ComY,TotM)
 
 class QTree():
     """
     Creates a QuadTree.
     
-    data   : x and y coordinates, must be 2D array of shape [n,2]      
+    data   : x and y coordinates, must be 2D array of shape [n,5]      
     k      : number of max points per node (integer)
     points
     """
     
     def __init__(self, data, k):        
         self.threshold = k
-        self.data      = [Body(data[i,0], data[i,1]) for i in range(len(data))]
+        self.bodies      = [Body(*[data[i,n] for n in range(data.shape[1])])                            
+                             for i in range(data.shape[0])]
         
-        self.X_dim    = (np.amax(data[:,0])-np.amin(data[:,0]))
-        self.Y_dim    = (np.amax(data[:,1])-np.amin(data[:,1]))
-        self.X_origin = np.amin(data[:,0])-0.1*self.X_dim
-        self.Y_origin = np.amin(data[:,1])-0.1*self.Y_dim
-        self.X_dim    = (np.amax(data[:,0])-self.X_origin)*1.1
-        self.Y_dim    = (np.amax(data[:,1])-self.Y_origin)*1.1
+        self.X_dim    = (amax(data[:,0])-amin(data[:,0]))
+        self.Y_dim    = (amax(data[:,1])-amin(data[:,1]))
+        self.X_origin = amin(data[:,0])-0.1*self.X_dim
+        self.Y_origin = amin(data[:,1])-0.1*self.Y_dim
+        self.X_dim    = (amax(data[:,0])-self.X_origin)*1.1
+        self.Y_dim    = (amax(data[:,1])-self.Y_origin)*1.1
         
-        self.root = Node(self.X_origin, self.Y_origin, self.X_dim, self.Y_dim, self.data)
+        self.root = Node(self.X_origin, self.Y_origin, self.X_dim, self.Y_dim, self.bodies)
         
         self.__subdivide()
 
@@ -58,11 +80,10 @@ class QTree():
         leaves = find_leaves(self.root)
         for n in leaves:
             ax.add_patch(patches.Rectangle((n.x0, n.y0), n.width, n.height, fill=False))
-        x = [point.x for point in self.data]
-        y = [point.y for point in self.data]
+        x = [point.x for point in self.bodies]
+        y = [point.y for point in self.bodies]
         plt.plot(x, y, 'ro')
         plt.show()
-        return
 
 # =============================================================================
 # Helper functions
@@ -87,7 +108,7 @@ def recursive_subdivide(node, k):
     se = Node(node.x0 + w_, node.y0, w_, h_, p)
     recursive_subdivide(se, k)
 
-    p = contains(node.x0+w_, node.y0+w_, w_, h_, node.points)
+    p = contains(node.x0+w_, node.y0+h_, w_, h_, node.points)
     ne = Node(node.x0+w_, node.y0+h_, w_, h_, p)
     recursive_subdivide(ne, k)
 
@@ -99,7 +120,7 @@ def contains(x, y, w, h, points):
     for point in points:
         if point.x >= x and point.x <= x+w and point.y>=y and point.y<=y+h:
             pts.append(point)
-    return pts
+    return(pts)
 
 def find_leaves(node):
     if not node.children:
@@ -108,7 +129,7 @@ def find_leaves(node):
         leaves = []
         for child in node.children:
             leaves += (find_leaves(child))
-    return leaves
+    return(leaves)
 
 # =============================================================================
 # Main
@@ -116,9 +137,11 @@ def find_leaves(node):
 
 
 def main():
+    from numpy import random
     
-    data = np.random.rand(1000,2)
-    QTree(data, 1).graph()
+    data = random.rand(6,5)
+    Q = QTree(data, 1)
+    return(data,Q)
 
 if __name__ == "__main__":
-    main()
+    data, Q = main()
